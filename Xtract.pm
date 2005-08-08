573ec60a65528ee20a01abeb34ca29c0cf775f28
Sunil S <sunils@cpan.org>
2005-08-08 23:25:19 +0000

    Import of SUNILS/PDF-Xtract-0.08 from CPAN.
    gitpan-cpan-distribution: PDF-Xtract
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         SUNILS/PDF/Xtract/008/PDF-Xtract-0.08.tar.gz
    gitpan-cpan-author:       SUNILS
    gitpan-cpan-maturity:     released

--- Xtract.pm
+++ Xtract.pm
@@ -8,13 +8,74 @@ use vars qw($VERSION);
 use File::Temp;
 # use Time::HiRes qw(gettimeofday);
 
-$VERSION = '0.02';
+$VERSION = '0.08';
 
-my ( $balance, $trailerObject, $RootObject, $EncryptedObject, $InfoObject, $TempExtractFile, $LastExtractFile );
+my ( $balance, $trailerObject, $RootObject, $EncryptedObject, $InfoObject, $TempExtractFile, $LastExtractFile, $CatalogPages);
 my ( %vars, %objval, %parent, %Referals, %IncludeObjects, %kids, %page, %BabyCountOfObject, %BabiesOfObject );
 
 my $CRLF = '[ \t\r\n\f\0]'."*(?:\015|\012|(?:\015\012))";
+# ---------------------------------------------------
+=head1 NAME
 
+PDF::Xtract - Extracting sub PDF documents from a multi page PDF document, much faster than PDF::Extract!
+
+=head1 SYNOPSIS
+
+	Please read Manual-PDF-Xtract.pdf in the distribution for detailed documentation.
+	
+	use PDF::Xtract;
+	$pdf=new PDF::Xtract;
+	@pages=(10..30,5,7); # Defining pages to be extracted ( 10 to 30, 5 and 7 - in the order required for output).
+	$pages=\@pages;
+	$pdf->savePDFExtract( PDFDoc=>"c:/Docs/my.pdf", PDFSaveAs="out.pdf", PDFPages=>$pages ); # Saves extracted pages to "out.pdf"
+
+	print "Content-Type text/plain\n\n<xmp>",  $pdf->getPDFExtract; # May be useful for a web-site!
+ 
+OR
+
+	# Extract and save, in the current directory, all the pages in a PDF document with nice names.
+	use PDF::Xtract;
+	$pdf=new PDF::Xtract( PDFDoc=>"test.pdf" );
+	@tmp=$pdf->getPDFExtractVariables(PDFPageCountIn);
+	$PageCount=${$tmp[0]};
+	print STDERR "Total Pages = $PageCount\n";
+	$tmp=length($PageCount);
+	for ( $CurPage=1; $CurPage <= $PageCount; $CurPage++ ) {
+		@CurPage=($CurPage); $CurRef=\@CurPage;
+		$index=sprintf("%0${tmp}d",$CurPage);
+		$pdf->savePDFExtract( PDFPages=>$CurRef,PDFSaveAs=>"$index.pdf" );
+	}
+
+=head1 DESCRIPTION
+
+PDF Xtract module is derived from Noel Sharrok's PDF::Extract module, but a MUCH faster one. It is a group
+of methods that allow the user to extract required pages as a new PDF document from a pre-existing PDF document.
+
+PDF::Xtract is published as a separate module, because of some significant differences with PDF::Extract in variables
+and functions implemented. While the code, for most part is a shameless copy of PDF::Extract, there are certain
+changes in the logic that allow this module to be much much faster with large PDF files.
+
+Notable differences between Xtract and Extract are also highlighted in this document
+
+With PDF::Xtract one can:-
+
+=over 4
+
+=item *
+Associate a PDF document to a PDF::Xtract object.
+
+=item *
+Get total number of pages in PDF document.
+
+=item *
+Extract required pages from a PDF document , as a new PDF document, in any specified page number order.
+
+=item *
+Specify name of file to save extracted PDF document.
+
+=back
+
+=cut
 # ----------------------------- The Public Methods --------------------------------
 
 # We can put the following 2 lines around a block to see the time taken to execute that.
@@ -26,20 +87,12 @@ sub new {
 	my $class = ref($this) || $this;
 	my $self = {};
 	bless $self, $class;
-	# Naming the temperory file for the object as xtract.tmp.CurrentHighResolutionTime.PPID
+	# Have a temperory file for storing output...
 	(undef,$TempExtractFile)=File::Temp->tempfile("xtract.tmp.XXXX");
-	# my @tmp=gettimeofday(); my $tmp=($tmp[0]+$tmp[1]/1000000); $TempExtractFile="xtract.tmp.$tmp.$$";
 	$vars{PDFErrorLevel}=3;
 	$self->setEnv(@_);
 	return $self;
 }
-
-#sub cleanup {
-#	# Delete temperory files only, user program probably should use this to clean-up the
-#	# mess this module might leave behind!
-#	close;
-#	unlink "$TempExtractFile" if ( -f "$TempExtractFile" );
-#}
 
 sub getPDFExtract{				
 	local undef $/;
@@ -105,6 +158,8 @@ sub setEnv {
 		foreach my $key (keys %PDF) { print STDERR "\t$key=\"$PDF{$key}\"\n"; }
 	}
 
+	if ( $PDF{PDFReadSize} ) { $vars{PDFReadSize}=$PDF{PDFReadSize} }
+	if ( $PDF{PDFWriteSize} ) { $vars{PDFWriteSize}=$PDF{PDFWriteSize} }
 	if ( $PDF{RetainPDFComments} ) { $vars{RetainPDFComments}=$PDF{RetainPDFComments} }
 	if ( $PDF{PDFVerbose} ) { $vars{PDFVerbose}=$PDF{PDFVerbose} }; # Put errors to STDERR too
 	if ( $PDF{PDFErrorLevel} ) { $vars{PDFErrorLevel}=$PDF{PDFErrorLevel} }; # Errors of higher gravity are
@@ -123,7 +178,6 @@ sub setEnv {
 		$vars{PDFPageCountIn}=$vars{PDFPageCountOut}=$vars{PDFPageCountErr}=undef;
 		$vars{PDFPagesFound}=$vars{PDFPagesNotFound}={};
 
-		my $CatalogPages;
 		unless ( -f "$PDF{PDFDoc}" ) { &error(3,"PDF document \"$PDF{PDFDoc}\" not found",__FILE__,__LINE__); }
 		my $tmp=join(undef,stat("$PDF{PDFDoc}"));
 		if ( $vars{PDFDocStat} eq $tmp ) { # SS: Y should you bother if the file is same!
@@ -134,27 +188,36 @@ sub setEnv {
 			if ( ! open FILE, "$PDF{PDFDoc}" ) {
 				&error(3,"Can't open PDF document  \"$PDF{PDFDoc}\" to read\n",__FILE__,__LINE__);		
 			} else {
-				local undef $/; binmode FILE; my $pdfFile=<FILE>; close FILE;
+				#----------------------------------
+				my $CarryOver; my $ChunkCount;
+				binmode FILE;
+				while(read FILE,my $str,$vars{PDFReadSize}?$vars{PDFReadSize}:1024000) {
+					$ChunkCount++; print STDERR "Reading chunk number $ChunkCount\r" if $vars{PDFVerbose};
+					my @tmp; my $pieces=(@tmp=split(/endobj\s*$CRLF/,"$CarryOver$str",-1)); undef $CarryOver;
+					my $count=0;
+					foreach $tmp (@tmp) {
+						$count++;
+						if ( $count == $pieces ) {
+							$CarryOver=$tmp; last;
+						} else {
+							if ( $tmp=~/^(.*?)(\d+)(\s+\d+\s+obj)$CRLF(.*)$/si ) {
+								$balance.=$1; my $obj=$2; $objval{$obj}="$obj$3\015\012$4endobj\015\012\015\012";
+								my $ref='(?:\s*\d+\s+\d+\s+R\s*)';
+								my @allRefs=($objval{$obj}=~/\/(\S+?[\[\s]+${ref}+[\]\s]?)/gs);
+								next unless @allRefs;
+								foreach ( @allRefs ) {
+									if 		( /^kids/is ) 	{ push (@{$kids{$obj}},&findRefs($_)); }
+									elsif 	( /^parent/is ) { $parent{$obj}=(&findRefs($_))[0] }
+									else 	{ push (@{$Referals{$obj}},&findRefs($_)); }
+								}
+							} else { $balance.=$tmp; }					
+						}
+					}
+				} $balance.=$CarryOver; print STDERR "\n" if $vars{PDFVerbose};
+				#----------------------------------
 				$vars{"PDFDoc"}=$PDF{"PDFDoc"};
 				# SS: Understand the document .....
 				#----------------------------------
-				my @tmp=split(/endobj\s*$CRLF/, $pdfFile); undef $pdfFile;
-				%kids=();	# Store kids for all parent objects.
-				foreach (@tmp) {
-					if ( /(.*?)(\d+)\s+(\d+)\s+obj(.*)/s ) {
-						$balance.=$1;
-						my $obj=$2; my $inst=$3; $objval{$obj}="$2 0 obj$4endobj\n";
-						if ( $objval{$obj}=~/\/Kids\s+\[\s*(.*?)\s+\]/s ) {
-							push @{$kids{$obj}},split(/\s+\d+\s+R\s*/,$1);
-						}
-						if ( $objval{$obj}=~/\/Parent\s+(\d+)\s+\d+\s+R/s ) {
-							$parent{$obj}=$1;
-						}
-						# Make a hash of arrays of referals.
-						my @refs=($objval{$obj}=~/\/[^(Root|Info|Pages|Parent|Kids|Enrypt)]\S+\s+(\d+)\s+\d+\s+R[^ALPHA]/gs);
-						push (@{$Referals{$obj}},@refs);
-					} else { $balance.=$_; }
-				}
 				# getTrailer, Info etc.
 				if ( $balance=~/(trailer\s*<<.*?>>\s*)/s ) {
 						$trailerObject=$1;
@@ -162,10 +225,10 @@ sub setEnv {
 						$trailerObject=~s/\/Prev.*?$CRLF//s;
 
 						if ( $trailerObject=~/\/Root\s+(\d+)\s+0\s+R/s ) 	{ $RootObject=$1 }
-						if ( $trailerObject=~/\/Encrypt\s+(\d+)\s+0\s+R/ ) 	{ $EncryptedObject=$1 }
-						if ( $trailerObject=~/\/Info\s+(\d+)\s+0\s+R/ ) 	{ $InfoObject=$1 }
+						if ( $trailerObject=~/\/Encrypt\s+(\d+)\s+0\s+R/s ) 	{ $EncryptedObject=$1 }
+						if ( $trailerObject=~/\/Info\s+(\d+)\s+0\s+R/s ) 	{ $InfoObject=$1 }
 				}	
-				if ( $objval{$RootObject}=~/\/Pages\s+(\d+)\s+0\s+R/ ) 	{ $CatalogPages=$1 }
+				if ( $objval{$RootObject}=~/\/Pages\s+(\d+)\s+0\s+R/s ) 	{ $CatalogPages=$1 }
 
 				&getPages($CatalogPages);
 			}
@@ -173,7 +236,6 @@ sub setEnv {
 	}
 
 	if ( exists $PDF{PDFSaveAs} ) {   # we also want to be able to set PDFSaveAs to nothing ("")	
-		# $PDF{PDFSaveAS}=~s/\.pdf$//i; # ******************
 		$vars{"PDFSaveAs"}=$PDF{"PDFSaveAs"};
 	}
 
@@ -192,7 +254,6 @@ sub setEnv {
 				&error(3,"Can't get PDF Pages. No page numbers were set with 'PDFPages' ",__FILE__,__LINE__);
 		}
 		@{$vars{PDFPages}}=@tmp;
-		# $vars{PDFPageCount}=$vars{PDFPagesFound}=$vars{PDFExtract}="";
 		$vars{PDFPagesFound}=""; # $vars{PDFExtract}="";
 		%IncludeObjects=(); # %IncludeObjects=undef;
 		%BabyCountOfObject=();
@@ -221,7 +282,6 @@ sub error {
 
 	my ($error_level,$error)=@_;
 	return 0 unless ( $error_level >= $vars{PDFErrorLevel} ); # Ignore those errors below set errorlevel
-	# my $size=$vars{PDFError}?scalar(@{$vars{PDFError}}):0;
 	if ( $vars{PDFError} && ( @{$vars{PDFError}} >= $vars{PDFErrorSize}) ) {
 		shift @{$vars{PDFError}};
 	}
@@ -250,6 +310,7 @@ sub Includes{
 	my @getRefs=@_;
 	foreach my $obj ( @getRefs ) {
 		foreach my $refered (@{$Referals{$obj}}) {
+			next if ( $IncludeObjects{$refered} );
 			$IncludeObjects{$refered}++;
 			&Includes($refered);
 		}
@@ -270,7 +331,7 @@ sub getPDFDoc {
 	foreach my $pageno ( @pickPages ) {
 		unless ( $page{$pageno} ) {	# if the page object is not in %page
 			$err++;
-			&error(2,"Page No. $pageno is not there in the given PDF file");
+			&error(2,"Page No. $pageno is not there in the given PDF file($vars{PDFDoc})");
 			$vars{PDFPageCountErr}++; push(@{$vars{PDFPagesNotFound}},$pageno);
 			next;
 		}
@@ -299,11 +360,7 @@ sub makePDF {
 		return 0;
 	}
 
-	# Decide the file to hold the extracted PDF Stream
-	$LastExtractFile=$vars{PDFSaveAs}?$vars{PDFSaveAs}:$TempExtractFile;
-	open(FILE,">$LastExtractFile") or die; binmode FILE;
-
-	if ( $vars{PDFClean}>0 ) {
+	if ( $vars{PDFClean} ) {
 		if ( $vars{PDFPageCountErr}>0 ) {
 			&error(3,"PDFClean: $vars{PDFPageCountErr} pages to be extracted were not found");
 			close FILE; unlink $LastExtractFile; return undef;
@@ -314,42 +371,85 @@ sub makePDF {
 		close FILE; unlink $LastExtractFile; return undef;
 	}
 
+	my $eol=sprintf "\n"; if ( length ($eol) <2 ) { $eol=" $eol" }
+
+	# Decide the file to hold the extracted PDF Stream
+	close FILE; # unlink "$LastExtractFile";  ####### 
+	$LastExtractFile=$vars{PDFSaveAs}?$vars{PDFSaveAs}:$TempExtractFile;
+	open(FILE,">$LastExtractFile") or die; binmode FILE;
+
+ 	my %XrefBlock=();
+	$objval{$RootObject}="$RootObject 0 obj\n<<\n/Type /Catalog\n/Pages $CatalogPages 0 R\n>>\nendobj\n";
 	$IncludeObjects{$RootObject}++; # print STDERR "Root ($RootObject) Added\n";
 	$IncludeObjects{$InfoObject}++; # print STDERR "Info ($InfoObject) Added\n";
 
 	(my $CurrentBalance=$balance)=~s/\%\%EOF.*//s;
-	$CurrentBalance=~s/(.*?$CRLF)//s;
+	$CurrentBalance=~s/^(.*?)$CRLF(.*?)$CRLF//s;
 
-	print FILE "$1$2";
-	# I dont know why ghostview complains if I dont keep the '-2' below!
-	my $startXref=length($1)-2;
+	print FILE "$1\015\012$2\015\012";
+	my $startXref=length("$1\015\015$2\015\012");
 
 	if ( $vars{RetainPDFComments} ) {
-		while ( $CurrentBalance=~/\s*(%.*?)($CRLF)/gs ) {
-			print FILE "$1$2";
-			$startXref+=length($1.$2);
+		while ( $CurrentBalance=~/\s*(%.*?$CRLF.*?$CRLF)/gs ) {
+			print FILE "$1";
+			$startXref+=length($1);
 		}
 	}
-	my $xref="0000000000 65535 f\015";
+	my $xref=undef;
+	my $xref="xxxxxxxxxx 65535 f$eol";
 	$vars{PDFObjCountOut}=undef;
 
+	my %addobj; my %objsize; my $accumulation; $vars{PDFWriteSize}=$vars{PDFWriteSize}?$vars{PDFWriteSize}:1024000; 
+	my $ExpectObjID=1;	# Next Object ID
+	my $lastObjID=undef; my $counter=0; my $CurXrefBlock=0;
+	$XrefBlock{0}[1]="0000000000 65535 f \015\012";
 	foreach my $objid ( sort {$a<=>$b} keys %IncludeObjects ) {
-		my $addobj=$objval{$objid};my $babiesofobject=undef;
-		foreach ( @{$BabiesOfObject{$objid}} ) { $babiesofobject.=" $_ 0 R"; }
+		$lastObjID=$objid;
+		$addobj{$objid}=$objval{$objid};my $babiesofobject=undef;
+		foreach ( @{$BabiesOfObject{$objid}} ) { $babiesofobject.="$_ 0 R "; }
 		if ( $babiesofobject ) {
-			$addobj=~s/\/Kids\s+.*?\]\s*/\/Kids \[$babiesofobject \]\n/s;
-			$addobj=~s/\/Count\s+\d+/\/Count $BabyCountOfObject{$objid}/s;
+			$addobj{$objid}=~s/\/Kids\s+.*?\]/\/Kids \[$babiesofobject\]/s;
+			$addobj{$objid}=~s/\/Count\s+\d+/\/Count $BabyCountOfObject{$objid}/s;
 		}
-		$xref.=sprintf("\012%0.10d %0.5d n", $startXref,0);
-		$startXref+=length($addobj);
-		print FILE "$addobj";
+
+		if ( int($objid) > $ExpectObjID ) {
+			my $tmp=$ExpectObjID;
+			for ( $ExpectObjID..int($objid)-1 ) {
+				$xref.="xxxxxxxxxx 00001 f$eol";
+				my $x=sprintf ("%0.10d",$ExpectObjID);
+				$xref=~s/xxxxxxxxxx/$x/;
+			}
+			$xref.=sprintf("%0.10d %0.5d n$eol",$startXref,0);
+		} else {
+			$xref.=sprintf("%0.10d %0.5d n$eol",$startXref,0);
+		}
+
+		$ExpectObjID=$objid+1;
+
+		$objsize{$objid}=length($addobj{$objid});
+		$startXref+=$objsize{$objid};
+		$accumulation+=$objsize{$objid};
+
+		if ( $accumulation > $vars{PDFWriteSize} ) {
+			foreach ( sort {$2<=>$b} keys %addobj ) { print FILE "$addobj{$_}"; delete $addobj{$_} };
+			$accumulation=0;
+			%addobj=();
+		}
 		$vars{PDFObjCountOut}++;
 	}
-	print FILE "xref\n0 $vars{PDFObjCountOut}\n$xref\015\012";
+	foreach ( sort {$a<=>$b} keys %addobj ) { print FILE "$addobj{$_}"; delete $addobj{$_} };
+	$lastObjID++;
 
-	(my $CurrentTrailer=$trailerObject)=~s/__Size__/$vars{PDFObjCountOut}/s;
-	print FILE "$CurrentTrailer\nstartxref\n$startXref\n\%\%EOF\n";
+	$xref=~s/xxxxxxxxxx/0000000000/;
+	print FILE "xref\0120 $lastObjID\n$xref";
+
+	(my $CurrentTrailer=$trailerObject)=~s/__Size__/$lastObjID/s;
+	print FILE "\015\012$CurrentTrailer\015\012startxref\015\012$startXref\015\012\%\%EOF\015\012";
 	close FILE;
+}
+
+sub findRefs{
+	return (/(?:(\d+)\s+\d+\s+R\s*)/gsi);
 }
 
 #sub lt{
@@ -395,6 +495,34 @@ PDF::Xtract - Extracting sub PDF documents from a multipage PDF document
 1;
 
 =head1 Notes
+
+
+Fri Jun 17 00:01:21 IST 2005
+
+Version 0.08
+
+Output does not open properly in Acrobat Reader properly !  Xref table writing changed back to
+what Noel was doing.
+
+Fri Apr 15 21:15:17 IST 2005
+
+Version 0.07
+
+Trying to accelerate output generation, which seems to be the remaining bottleneck. 
+
+Thu Apr  7 15:02:22 IST 2005
+
+Version 0.06
+
+Noticed that Xtract fails with very large PDFs (>400MB).  It is now fixed by changing the way the file is read and
+understood.  Fringe benefit: module uses less memory than before.  Additional variable is introduced: PDFReadSize,
+specify the number of bytes to read at a time when reading the input file.
+
+Thu Mar  10 15:02:47 IST 2005
+
+Noticed a problem with include objects!  Work around done.
+
+Thu Feb  20 15:02:47 IST 2005
 
 Operational sequences within the module is being changed.  New organisation will be as below:
 
